a23c769a86473dc6529a58a14c72db18f1a6e0c6
Sepehr <sepehr.dh99@gmail.com>
2025-08-25 14:37:13 +0330

    fix: phone number dto validation

--- controller/dto/auth.go
+++ controller/dto/auth.go
@@ -1,11 +1,11 @@
 package dto
 
 type RequestOTPRequest struct {
-	Phone string `json:"phone" example:"09123456789" validate:"required,e164,startswith=09,len=11"`
+	Phone string `json:"phone" example:"09123456789" validate:"required,startswith=09,len=11"`
 }
 
 type VerifyOTPRequest struct {
-	Phone string `json:"phone" example:"09123456789" validate:"required,e164,startswith=09,len=11"`
+	Phone string `json:"phone" example:"09123456789" validate:"required,startswith=09,len=11"`
 	OTP   string `json:"otp" example:"1234" validate:"required,len=6,numeric"`
 }
 
